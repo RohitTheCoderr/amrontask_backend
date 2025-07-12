c3f7571f775d32ff5c5d024c29bf835bcb3d42c6
Rohit kumar <rohitkumar9643017@gmail.com>
2025-07-12 23:24:23 +0530

    update cors

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
-import express from "express"
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 import userRoutes from "./routes/userRouter.js";
 import adminRouter from "./routes/adminRouter.js";
 import productsRouter from "./routes/productsRouter.js";
-import { connectDatabase } from './config/db.js';
-import cors from 'cors';
+import { connectDatabase } from "./config/db.js";
+import cors from "cors";
 
 dotenv.config();
 connectDatabase();
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000', // allow frontend
-  credentials: true
-}));
+app.use(
+  cors({
+    // origin: 'http://localhost:3000', // allow frontend
+    origin: [
+      // allow frontend
+      "https://mynextjsproject.netlify.app",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  })
+);
+
+// const allowedOrigins = [
+//   'http://localhost:3000',               // Dev frontend
+//   'https://your-frontend.vercel.app'     // Replace with your actual frontend domain
+// ];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     // allow requests with no origin (like curl or Postman) or matching allowed list
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true
+// }));
 
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
-app.use('/api/admin', adminRouter);
-app.use('/api/products', productsRouter);
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRouter);
+app.use("/api/products", productsRouter);
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 const PORT = process.env.PORT || 5000;
